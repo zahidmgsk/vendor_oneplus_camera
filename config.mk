6d7dfd32ce1351a60b21cd9ac9f62efabe003620
root <root@zahidm.devworld.com>
2020-11-14 17:02:58 +0100

    Nuke OP Cam add OP Gallery

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 PRODUCT_PACKAGES += \
-    OnePlusCamera \
-    OnePlusCameraService \
     OnePlusGallery
+#    OnePlusCamera \
+#    OnePlusCameraService
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
